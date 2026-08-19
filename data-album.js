25943dc360d6834e0fd1500ab029cdb83795d29f
bintangmenggala0-creator <bintangmenggala0@gmail.com>
2026-08-19 21:59:21 +0700

    Add files via upload

---
+++ data-album.js
@@ -0,0 +1,108 @@
+
+const albumData = [
+    {
+        type: "video",
+        category: "OPHAR",
+        title: "OLT Marga Pundu",
+        desc: "Dokumentasi video proses penanganan perangkat OLT di wilayah Marga Pundu.",
+        src: "https://res.cloudinary.com/buklippb/video/upload/v1785398112/Olt_Marga_Pundu_m4j0b8.mp4"
+    },
+    {
+        type: "video",
+        category: "OPHAR",
+        title: "OLT Metro",
+        desc: "Dokumentasi kegiatan pemeliharaan dan pembersihan OLT Metro.",
+        src: "https://res.cloudinary.com/buklippb/video/upload/v1785398024/oltmetro_itchna.mp4"
+    },
+    {
+        type: "video",
+        category: "OPHAR",
+        title: "Perjalanan Margapundu",
+        desc: "Dokumentasi perjalanan menuju lokasi wilayah Marga Pundu dalam rangka maintenance jaringan.",
+        src: "https://res.cloudinary.com/buklippb/video/upload/v1785513470/perjalananmargapundu_mgh7ov.mp4"
+    },
+    {
+        type: "video",
+        category: "OPHAR",
+        title: "Perjalanan Metro",
+        desc: "Dokumentasi perjalanan menuju wilayah Metro untuk pemeliharaan jaringan.",
+        src: "https://res.cloudinary.com/buklippb/video/upload/v1785513471/videoperjalanametro_q1xiue.mp4"
+    },
+    {
+        type: "image",
+        category: "OPHAR",
+        title: "OLT wilayah Margapundu",
+        desc: "Inspeksi dan maintenance perangkat Optical Line Terminal di wilayah Margapundu untuk memastikan kestabilan koneksi serat optik.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785399468/Foto1oltmargapundu_vfq7pv.jpg"
+    },
+    {
+        type: "image",
+        category: "OPHAR",
+        title: "OLT wilayah Metro",
+        desc: "Inspeksi dan pemeliharaan perangkat OLT di wilayah metro.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785399467/Foto2oltmetro_j9qgdm.jpg"
+    },
+    {
+        type: "image",
+        category: "OPHAR",
+        title: "FAT Rajabasa",
+        desc: "Inspeksi serta penataan atau Fiber Access Terminal (FAT) di wilayah Rajabasa.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785399470/Foto3fatrajabasa_bijyoj.jpg"
+    },
+    {
+        type: "image",
+        category: "Kegiatan pembelajaran",
+        title: "Perjalanan menuju Labuhan Maringgai",
+        desc: "Perjalanan menuju Labuhan Maringgai guna partisipasi dan canvassing",
+        src: "https://res.cloudinary.com/buklippb/image/upload/v1787150681/IMG-20260818-WA0015_n76czk.jpg"
+    },
+    {
+        type: "image",
+        category: "Infrastruktur",
+        title: "Rumija Jatiagung",
+        desc: "Pendataan tiang di area Ruang Milik Jalan (Rumija) di wilayah Jatiagung.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785479756/TimePhoto_20260731_095847_1_rob9yk.jpg"
+    },
+    {
+        type: "image",
+        category: "Infrastruktur",
+        title: "Pengukuran Rumija Jatiagung",
+        desc: "Proses pengukuran tiang di area Ruang milik jalan (RUMIJA) wilayah Jatiagung guna pendataan.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785479731/TimePhoto_20260731_100059_1_pczkvr.jpg"
+    },
+    {
+        type: "image",
+        category: "Infrastruktur",
+        title: "Pengukuran Rumija Jatiagung",
+        desc: "Dokumentasi lanjutan pengukuran jarak dan titik tiang pada area Rumija wilayah Jatiagung.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785479714/TimePhoto_20260731_095947_1_cqvot4.jpg"
+    },
+    {
+        type: "image",
+        category: "Kegiatan pembelajaran",
+        title: "Mikrotik dan Topologi",
+        desc: "Pembelajaran materi mikrotik dan topologi, Serta konfigurasi LAN melalu GUI.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/v1785989604/IMG20260806103242_zzsyyw.jpg"
+    },
+    {
+        type: "image",
+        category: "Kegiatan pembelajaran",
+        title: "Canvassing",
+        desc: "Kegiatan Canvassing dan sosialisasi Produk Icon Plus.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/v1787149036/TimePhoto_20260812_152650_ajk2cv.jpg"
+    },
+    {
+        type: "image",
+        category: "Kegiatan pembelajaran",
+        title: "Canvassing",
+        desc: "Canvassing di wilayah Labuhan Maringgai",
+        src: "https://res.cloudinary.com/buklippb/image/upload/v1787149186/TimePhoto_20260818_135347_sqz711.jpg"
+    },
+    {
+        type: "image",
+        category: "Kegiatan pembelajaran",
+        title: "Canvassing",
+        desc: "Canvassing door to door di wilayah kedamaian.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/v1787150087/TimePhoto_20260807_111912_gal26z.jpg"
+    }
+];
