e7d571f00767fc15181f388421a663f2615f1557
bintangmenggala0-creator <bintangmenggala0@gmail.com>
2026-09-12 03:08:09 +0700

    Add files via upload

---
+++ data-album.js
@@ -0,0 +1,48 @@
+
+const albumData = [
+    {
+        type: "video",
+        category: "OPHAR",
+        title: "OLT Marga Pundu",
+        desc: "Dokumentasi video proses penanganan perangkat OLT di wilayah Marga Pundu.",
+        src: "https://res.cloudinary.com/buklippb/video/upload/v1785398112/Olt_Marga_Pundu_m4j0b8.mp4"
+    },
+    {
+        type: "video",
+        category: "OPHAR",
+        title: "OLT Metro",
+        desc: "Dokumentasi kegiatan pemeliharaan dan pembersihan OLT Metro.",
+        src: "https://res.cloudinary.com/buklippb/video/upload/v1785398024/oltmetro_itchna.mp4"
+    },
+    
+    {
+        type: "image",
+        category: "OPHAR",
+        title: "OLT wilayah Margapundu",
+        desc: "Inspeksi dan maintenance perangkat Optical Line Terminal di wilayah Margapundu untuk memastikan kestabilan koneksi serat optik.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785399468/Foto1oltmargapundu_vfq7pv.jpg"
+    },
+    {
+        type: "image",
+        category: "OPHAR",
+        title: "OLT wilayah Metro",
+        desc: "Inspeksi dan pemeliharaan perangkat OLT di wilayah metro.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785399467/Foto2oltmetro_j9qgdm.jpg"
+    },
+    {
+        type: "image",
+        category: "OPHAR",
+        title: "FAT Rajabasa",
+        desc: "Inspeksi serta penataan atau Fiber Access Terminal (FAT) di wilayah Rajabasa.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785399470/Foto3fatrajabasa_bijyoj.jpg"
+    },
+    
+    {
+        type: "image",
+        category: "Kegiatan pembelajaran",
+        title: "Mikrotik dan Topologi",
+        desc: "Pembelajaran materi mikrotik dan topologi, Serta konfigurasi LAN melalu GUI.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/v1785989604/IMG20260806103242_zzsyyw.jpg"
+    },
+     
+];
