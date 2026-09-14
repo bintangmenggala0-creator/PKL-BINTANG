e2fbfc058260e63f09479423c2e6ce20d762e9db
bintangmenggala0-creator <bintangmenggala0@gmail.com>
2026-09-15 02:38:53 +0700

    Delete page-init.js

--- page-init.js
+++
@@ -1,7 +0,0 @@
-if (sessionStorage.getItem("introShown") === "true") {
-    document.documentElement.classList.add("intro-done");
-}
-
-if (sessionStorage.getItem("pageFade") === "1") {
-    document.documentElement.classList.add("from-transition");
-}
