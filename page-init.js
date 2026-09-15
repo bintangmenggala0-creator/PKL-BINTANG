7474e0a5bc3c07676a9a29ae0f88bfe42e3fab13
bintangmenggala0-creator <bintangmenggala0@gmail.com>
2026-09-16 00:37:45 +0700

    Add files via upload

---
+++ page-init.js
@@ -0,0 +1,8 @@
+
+if (sessionStorage.getItem("introShown") === "true") {
+    document.documentElement.classList.add("intro-done");
+}
+
+if (sessionStorage.getItem("pageFade") === "1") {
+    document.documentElement.classList.add("from-transition");
+}
