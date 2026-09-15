6bf17149e4a48e07192fdea99af38539f5304f74
bintangmenggala0-creator <bintangmenggala0@gmail.com>
2026-09-16 02:05:23 +0700

    Add files via upload

---
+++ data-album.js
@@ -0,0 +1,48 @@
+
+const albumData = [
+    {
+        type: "video",
+        category: "OPHAR",
+        title: "Margapundu",
+        desc: "Maintenance dan Troubleshooting OLT Outdoor pole-mounted di wilayah Marga Pundu.",
+        src: "https://res.cloudinary.com/buklippb/video/upload/v1785398112/Olt_Marga_Pundu_m4j0b8.mp4"
+    },
+    {
+        type: "video",
+        category: "OPHAR",
+        title: "Metro",
+        desc: "Preventive Maintenance Ground-Mounted OLT Outdoor Cabinet di wilayah Metro. ",
+        src: "https://res.cloudinary.com/buklippb/video/upload/v1785398024/oltmetro_itchna.mp4"
+    },
+    
+    {
+        type: "image",
+        category: "OPHAR",
+        title: "Margapundu",
+        desc: "Preventive Maintenance OLT Pole-Mounted yang meliputi Pemeriksaan redaman, penggantian Patch Cord, dan input Voltage. ",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785399468/Foto1oltmargapundu_vfq7pv.jpg"
+    },
+    {
+        type: "image",
+        category: "OPHAR",
+        title: "Metro",
+        desc: "Preventive Maintenance OLT yang meliputi mengukur arus DC, pemeriksaan tegangan PSU, dan membersihkan Kabinet.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785399467/Foto2oltmetro_j9qgdm.jpg"
+    },
+    {
+        type: "image",
+        category: "OPHAR",
+        title: "Rajabasa",
+        desc: "Preventive maintenance FAT yang meliputi pemeriksaan splitter, dan redaman.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/f_auto,q_auto/v1785399470/Foto3fatrajabasa_bijyoj.jpg"
+    },
+    
+    {
+        type: "image",
+        category: "Kegiatan pembelajaran",
+        title: "Mikrotik dan Topologi",
+        desc: "Mempelajari materi tentang konfigurasi Mikrotik melalui graphical user interface dan Variasi dari topologi Local Area Network.",
+        src: "https://res.cloudinary.com/buklippb/image/upload/v1785989604/IMG20260806103242_zzsyyw.jpg"
+    },
+     
+];
